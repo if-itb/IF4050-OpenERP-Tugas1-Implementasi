cbf2cb49b3d3cfb654ddacb2931e5a2d01be8739
Andy Primawan <andyprimawan@gmail.com>
2014-11-26 18:10:43 +0700

    Ini masukkin modul OpenAcademy

---
+++ openacademy/__openerp__.py
@@ -0,0 +1,19 @@
+{
+    'name': 'Open Academy',
+    'version': '1.0',
+    'category': 'Tools',
+    'summary': 'Courses, Sessions, Subscriptions',
+    'description': """
+Courses, Sessions, Subscriptions
+================================
+
+This module manages the definition of course, the organization,
+sessions, and the subscriptions of participants.
+""",
+    'depends' : ['base'],
+    'data' : ['view/menu.xml'],
+    'images': [],
+    'demo': [],
+    'installable' : True,
+    'application': True,
+}
